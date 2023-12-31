191deedcd0f94fe2dc44809e47a3225e4a00ef72
Hu Bowen <contact@hubowen.dev>
2023-12-31 22:01:33 +0800

    chore: cleanup file

--- qrandom.py
+++ qrandom.py
@@ -1,5 +1,3 @@
-import numpy as np
-
 from quantum.base import quantum_register, measure_all
 from quantum.gates import H
 
@@ -8,6 +6,3 @@ def random_bits(num):
     for i in range(num):
         reg = H(i)(reg)
     return measure_all()(reg)
-
-for i in range(10):
-    print("".join(map(str, random_bits(10))))
