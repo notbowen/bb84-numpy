feba37802b2216a41c857ec02dbfc72f4d286c29
Hu Bowen <contact@hubowen.dev>
2024-01-07 10:56:32 +0800

    feat: added SQP

---
+++ protocol.py
@@ -0,0 +1,27 @@
+class SQPMessage:
+    def __init__(self, msg=None) -> None:
+        self.method = ""
+        self.target = ""
+        self.sender = ""
+
+        self.data = ""
+
+        if msg:
+            self.parse(msg)
+
+    def create(self, method: str, target: str, sender: str, data: str) -> None:
+        self.method = method
+        self.target = target
+        self.sender = sender
+        self.data = data
+
+    def parse(self, msg: bytes) -> None:
+        msg_decoded = msg.decode()
+        header, self.data = msg_decoded.split("\n\n")
+
+        dest_info, self.sender = header.splitlines()
+        self.method, self.target = dest_info.split(" ")
+
+    def to_bytes(self) -> bytes:
+        return f"{self.method} {self.target}\n{self.sender}\n\n{self.data}".encode()
+
